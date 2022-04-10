0e008da1e017b056fb45a83590061ed8624adeb8
djeriko <jerikosalivan@gmail.com>
2022-04-11 02:25:21 +0300

    Improve solution in task #5

--- 5_longest_palindromic_substring/solution.py
+++ 5_longest_palindromic_substring/solution.py
@@ -15,18 +15,47 @@ class Solution:
         return best_s
 
     def longestPalindromeBrute2(self, s: str) -> str:
+        """Complexity is O^3."""
         for length in range(len(s), 0, -1):
             for start_index in range(0, len(s)+1-length):
                 if self.isPalindrome(s[start_index:(start_index+length)]):
                     return s[start_index:(start_index+length)]
 
-    def growFromCenter(self, s: str):
+    def longestPalindrome(self, s: str):
+        """Complexity is O^2."""
         biggest = s[0]
-        step = len(biggest)//2
+        step = 0
+        
+        # Handle single letter center. Odd case.
+        for center in range(1, len(s)-1):
+            bounds = [center-(1+step), center+(1+step)]
+            while (bounds[0] > -1) and (bounds[1] < len(s)):
+                if self.isPalindrome(s[bounds[0]:bounds[1]+1]):
+                    biggest = s[bounds[0]:bounds[1]+1]
+                    step = len(biggest)//2
+                    bounds[0] -= 1
+                    bounds[1] += 1
+                else:
+                    break
+        # Handle double letter center. Even case.
+        for center in range(step, len(s)-step-1):
+            bounds = [center-(step), center+(1+step)]
+            while (bounds[0] > -1) and (bounds[1] < len(s)):
+                if self.isPalindrome(s[bounds[0]:bounds[1]+1]):
+                    biggest = s[bounds[0]:bounds[1]+1]
+                    step = len(biggest)//2
+                    bounds[0] -= 1
+                    bounds[1] += 1
+                else:
+                    break
+
+        return biggest    
+            
 
                 
 
-    def longestPalindrome(self, s:str) -> str:
+    def longestPalindrome1(self, s:str) -> str:
+        """Complexity is O^2."""
         best_len = 0
         best_s = ""
         for mid in range(len(s)):
