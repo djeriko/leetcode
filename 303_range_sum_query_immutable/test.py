9c2ca1a6ef7e4a6fc02716e8e29c6405cc726d8d
djeriko <jerikosalivan@gmail.com>
2022-04-24 20:51:01 +0300

    Resolve problem

--- 303_range_sum_query_immutable/test.py
+++ 303_range_sum_query_immutable/test.py
@@ -3,15 +3,30 @@ from solution import NumArray
 
 class TestSolution(unittest.TestCase):
     def setUp(self):
-        self.numArray = NumArray([1, 2])
-
+        nums = [-2, 0,  3, -5, 2, -1]
+        self.numArray = NumArray(nums)
+    
     def test_1(self):
-        left = 1
-        right = 1
+        left = 0
+        right = 2
         
         result = self.numArray.sumRange(left, right)
-        expected = [None, 1, -1, -3]
-        self.assertEqual(result, expected)
+        self.assertEqual(result, 1)
+
+    def test_2(self):
+        left = 2
+        right = 5
+        
+        result = self.numArray.sumRange(left, right)
+        self.assertEqual(result, -1)
+
+    def test_3(self):
+        left = 0
+        right = 5
+        
+        result = self.numArray.sumRange(left, right)
+        self.assertEqual(result, -3)
+        
 
 if __name__ == "__main__":
     unittest.main()
