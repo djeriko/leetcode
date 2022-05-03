ab9c11bcba83ebdad91e12f58eed2d665b19cbc9
djeriko <jerikosalivan@gmail.com>
2022-05-03 20:54:54 +0300

    Resovle with too big space price

--- 920_meeting_rooms/solution.py
+++ 920_meeting_rooms/solution.py
@@ -7,4 +7,16 @@ class Interval(object):
 
 class Solution:
     def canAttendMeeting(self, intervals: List[Interval]) -> bool:
-        pass
+        """
+        Time: O(n)
+        Space: O(len(max(n))
+        Not pass space limit
+        """
+        time = set()
+        for interval in intervals:
+            for i in range(interval.start, interval.end, 1):
+                if (i not in time):
+                    time.add(i)
+                else:
+                    return False
+        return True
--- 920_meeting_rooms/test.py
+++ 920_meeting_rooms/test.py
@@ -1,15 +1,34 @@
 import unittest
-from solution import Solution
+from solution import Solution, Interval
 
 class TestSolution(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
     def test_1(self):
-        arr = [(0,30), (5,10), (15,20)]
+        arr = [Interval(0,30), Interval(5,10), Interval(15,20)]
         result = self.solution.canAttendMeeting(arr)
-        
+
         self.assertEqual(result, False)
+
+    def test_2(self):
+        arr = [Interval(5,8), Interval(9,15)]
+        result = self.solution.canAttendMeeting(arr)
+
+        self.assertEqual(result, True)
+
+    def test_3(self):
+        arr = [Interval(465,497),Interval(386,462),Interval(354,380),Interval(134,189),Interval(199,282),Interval(18,104),Interval(499,562),Interval(4,14),Interval(111,129),Interval(292,345)]
+        result = self.solution.canAttendMeeting(arr)
+
+        self.assertEqual(result, True)
+
+    def test_4(self):
+        arr = [Interval(0,8), Interval(8,10)]
+        result = self.solution.canAttendMeeting(arr)
+
+        self.assertEqual(result, True)
+        
 
 if __name__ == "__main__":
     unittest.main()
