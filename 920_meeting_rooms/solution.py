0b504009d4f3190fa6859b55e8a71512cd60a23a
djeriko <jerikosalivan@gmail.com>
2022-05-07 16:15:25 +0300

    Add more afficient solution

--- 920_meeting_rooms/solution.py
+++ 920_meeting_rooms/solution.py
@@ -1,12 +1,14 @@
 from typing import List
 
+
 class Interval(object):
     def __init__(self, start, end):
         self.start = start
         self.end = end
 
+
 class Solution:
-    def canAttendMeeting(self, intervals: List[Interval]) -> bool:
+    def canAttendMeeting_1(self, intervals: List[Interval]) -> bool:
         """
         Time: O(n)
         Space: O(len(max(n))
@@ -19,4 +21,17 @@ class Solution:
                     time.add(i)
                 else:
                     return False
+        return True
+
+    def canAttendMeeting(self, intervals: List[Interval]) -> bool:
+        """
+        Time: O(n*log(n))
+        Space: O(1)
+        """
+        intervals = sorted(intervals, key=lambda x: x.start)
+
+        for i in range(len(intervals) - 1):
+            if intervals[i].end > intervals[i+1].start:
+                return False
+
         return True
