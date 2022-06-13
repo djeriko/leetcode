56ba44a361201b34f7309b9ed9ba8491401aa029
djeriko <jerikosalivan@gmail.com>
2022-06-13 13:48:04 +0300

    Problem solved

--- 1_two_sum/solution.py
+++ 1_two_sum/solution.py
@@ -1,5 +1,15 @@
-
 class Solution:
+    '''
+    Time - O(n)
+    Space - O(n)
+    Pattern - HashTable
+    '''
     def twoSum(self, nums: [], target: int):
-        pass
+        seen = {}
+        for i in range(len(nums)): 
+            if target - nums[i] in seen:
+                return [seen[target - nums[i]] ,i]
+            seen[nums[i]] = i
+        
+        return []
 
